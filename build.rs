2e7c0085499eca31b29891005cee353330932d3c
Keziah Biermann <development@kizzycode.de>
2024-02-19 00:54:33 +0100

    More relaxed warning behavior for SQLite (we trust this dependency)

--- build.rs
+++ build.rs
@@ -2,13 +2,13 @@ use cc::Build;
 
 fn main() {
     Build::new()
-        .warnings_into_errors(true)
+        .extra_warnings(true)
         // Recommended flags; see https://www.sqlite.org/compile.html
         .flag("-DSQLITE_DQS=0")
         .flag("-DSQLITE_DEFAULT_MEMSTATUS=0")
         .flag("-DSQLITE_DEFAULT_WAL_SYNCHRONOUS=1")
         .flag("-DSQLITE_OMIT_DEPRECATED")
-        //.flag("-DSQLITE_OMIT_SHARED_CACHE") -- causes warnings if enabled
+        .flag("-DSQLITE_OMIT_SHARED_CACHE")
         .flag("-DSQLITE_STRICT_SUBTYPE=1")
         // Build lib
         .include("dist/")
